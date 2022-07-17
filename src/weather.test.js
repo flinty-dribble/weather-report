269dfb82c348ddd16d60d33f70d7fa5a93821b3a
Mirab <you@example.com>
2022-07-17 20:58:29 +0300

    response body

--- src/weather.test.js
+++ src/weather.test.js
@@ -1,18 +1,17 @@
 import { getWeather } from "./weather";
 
-beforeEach(() => {
-  fetch.resetMocks();
-});
-
 describe("weather report", () => {
   it("user`s weather", async () => {
-    fetch.mockResponses(
-      [JSON.stringify({ latitude: 39 }), { status: 200 }],
-      [JSON.stringify({ main: { temp: 290 } }), { status: 200 }]
-    );
+    const asyncMock = jest
+      .fn()
+      .mockResolvedValue("default")
+      .mockResolvedValueOnce(
+        (await fetch("https://get.geojs.io/v1/ip/geo.json")).json()
+      )
+      .mockResolvedValueOnce(getWeather());
 
-    const res = await getWeather();
-    expect(res).toEqual(290);
+    expect(await asyncMock()).toEqual(290);
+
     // expect(document.querySelector(".city").innerHTML).toBe("city: Moscow");
     // expect(document.querySelector(".temp").innerHTML).toBe("temperature: 20");
     // expect(document.querySelector("img").src).toBe(
