1cd1d87cc3d39fd99147ff9687062b04ff0e4e38
Mirab <you@example.com>
2022-07-21 09:49:52 +0300

    problems with cities

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,12 @@
 export async function getWeather() {
   try {
+    const geojs = await fetch("https://get.geojs.io/v1/ip/geo.json");
+    const pos = await geojs.json();
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&appid=396e05fd2171ceaefead7acce7feab44`
+    );
+    const text = await response.json();
+
     const body = document.querySelector("body");
     const city = document.createElement("p");
     const temp = document.createElement("p");
@@ -11,19 +18,62 @@ export async function getWeather() {
     body.append(city);
     body.append(temp);
     body.append(icon);
-    const geojs = await fetch("https://get.geojs.io/v1/ip/geo.json");
-    const pos = await geojs.json();
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&appid=396e05fd2171ceaefead7acce7feab44`
-    );
-    const text = await response.json();
 
     city.innerHTML = `city: ${text.name}`;
     temp.innerHTML = `temperature: ${Math.round(text.main.temp - 273.15)}`;
     icon.src = `http://openweathermap.org/img/wn/${text.weather[0].icon}@2x.png`;
-    return pos.latitude;
+    return text;
   } catch (err) {
     return err;
   }
 }
+
 getWeather();
+
+const input = document.createElement("input");
+const button = document.createElement("button");
+button.innerHTML = "click";
+const body = document.querySelector("body");
+body.append(input);
+body.append(button);
+
+export async function weatherOfCity() {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=396e05fd2171ceaefead7acce7feab44`
+  );
+  const result = await response.json();
+  return result;
+}
+
+button.addEventListener("click", async () => {
+  const weather = await weatherOfCity();
+
+  const city = document.querySelector(".city");
+  const temp = document.querySelector(".temp");
+  const icon = document.querySelector("img");
+
+  city.innerHTML = `city: ${weather.name}`;
+  temp.innerHTML = `temperature: ${Math.round(weather.main.temp - 273.15)}`;
+  icon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+
+  const town = document.createElement("p");
+  town.classList.add("history");
+  const history = document.querySelectorAll(".history");
+
+  if (history.length >= 1) {
+    for (let i = 0; i < history.length; i += 1) {
+      if (input.value === history[i].innerHTML) {
+        history[i].remove();
+      }
+    }
+  }
+
+  if (history.length >= 10) {
+    history[0].remove();
+    body.append(town);
+    town.innerHTML = `${input.value}`;
+  } else {
+    body.append(town);
+    town.innerHTML = `${input.value}`;
+  }
+});
--- src/weather.test.js
+++ src/weather.test.js
@@ -1,21 +1,104 @@
-import { getWeather } from "./weather";
+import { getWeather, weatherOfCity } from "./weather";
 
 describe("weather report", () => {
   it("user`s weather", async () => {
+    global.fetch = jest
+      .fn()
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          json: () => Promise.resolve({}),
+        })
+      )
+      .mockImplementationOnce(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({
+              name: "Moscow",
+              main: { temp: 291.15 },
+              weather: [{ icon: "03d" }],
+            }),
+        })
+      );
+
+    const res = await getWeather();
     const asyncMock = jest
       .fn()
-      .mockResolvedValue("default")
-      .mockResolvedValueOnce(
-        (await fetch("https://get.geojs.io/v1/ip/geo.json")).json()
-      )
-      .mockResolvedValueOnce(getWeather());
+      .mockResolvedValue(
+        `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
+      );
+    expect(document.querySelector(".city").innerHTML).toBe(`city: ${res.name}`);
+    expect(document.querySelector(".temp").innerHTML).toBe(
+      `temperature: ${res.main.temp - 273.15}`
+    );
+    expect(document.querySelector("img").src).toBe(await asyncMock());
+  });
 
-    expect(await asyncMock()).toEqual(290);
+  const input = document.querySelector("input");
+  const button = document.querySelector("button");
 
-    // expect(document.querySelector(".city").innerHTML).toBe("city: Moscow");
-    // expect(document.querySelector(".temp").innerHTML).toBe("temperature: 20");
-    // expect(document.querySelector("img").src).toBe(
-    //  "http://openweathermap.org/img/wn/03d@2x.png"
-    // );
+  it("input and button", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            name: "London",
+            main: { temp: 295.15 },
+            weather: [{ icon: "01d" }],
+          }),
+      })
+    );
+
+    input.value = "London";
+    button.dispatchEvent(new Event("click"));
+
+    const res = await weatherOfCity();
+
+    const asyncMock = jest
+      .fn()
+      .mockResolvedValue(
+        `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
+      );
+    expect(document.querySelector(".city").innerHTML).toBe(`city: ${res.name}`);
+    expect(document.querySelector(".temp").innerHTML).toBe(
+      `temperature: ${res.main.temp - 273.15}`
+    );
+    expect(document.querySelector("img").src).toBe(await asyncMock());
+  });
+
+  it("history", () => {
+    const history = document.querySelectorAll(".history");
+
+    input.value = "Washington";
+    button.dispatchEvent(new Event("click"));
+    expect(history[0].innerHTML).toBe("Washington");
+    input.value = "Tokyo";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Paris";
+    button.dispatchEvent(new Event("click"));
+    input.value = "New York";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Los Angeles";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Beijing";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Baku";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Sochi";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Nepal";
+    button.dispatchEvent(new Event("click"));
+    input.value = "Rio";
+    button.dispatchEvent(new Event("click"));
+    // expect(cities[4].innerHTML).toBe(input.value)
+    // expect(history).toBe(input.value);
+
+    const secondCity = history[1];
+    input.value = "Hokkaido";
+    button.dispatchEvent(new Event("click"));
+    expect(history[0].innerHTML).toBe(secondCity.innerHTML);
+
+    input.value = "Paris";
+    button.dispatchEvent(new Event("click"));
+    expect(history[9].innerHTML).not.toBe(input.value);
   });
 });
